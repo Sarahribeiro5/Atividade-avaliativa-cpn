054ab339dc0ec447737b0d5fd482215ccb71a650
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2025-04-03 16:23:01 -0300

    feat: add project

---
+++ src/app/components/feed/index.jsx
@@ -0,0 +1,29 @@
+import styles from "./feed.module.css";
+
+const Feed = () => {
+    return (
+        <section className={styles.feedSection}>
+            <div className={styles.feedHeader}>
+              <h2 className={styles.sectionTitle}>Memes Populares</h2>
+              <div className={styles.feedFilters}>
+                <button className={`${styles.filterButton} ${styles.active}`}>
+                  Recentes
+                </button>
+                <button className={styles.filterButton}>Mais curtidos</button>
+                <button className={styles.filterButton}>Mais comentados</button>
+              </div>
+            </div>
+
+            <div className={styles.feedGrid}>
+              {/* Aqui mapeamos os memes do array para criar múltiplos cards */}
+              {memes.map((meme) => (<InteractionBar meme={meme} />))}
+            </div>
+
+            <button className={styles.loadMoreButton}>
+              Carregar mais memes
+            </button>
+          </section>
+    );
+}
+
+export default Feed;
